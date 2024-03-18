654bb5c83576d1076840cd562c8947b0b7104c67
胖橘 <nullptr255@outlook.com>
2024-03-18 15:07:49 +0800

    perf(URIUtils): 优化URIUtils

---
+++ pangju-framework-web/src/main/java/io/github/pangju666/framework/web/utils/UriUtils.java
@@ -0,0 +1,35 @@
+package io.github.pangju666.framework.web.utils;
+
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class UriUtils {
+	protected UriUtils() {
+	}
+
+	public static URI fromHttpUrl(String httpUrl) {
+		return UriComponentsBuilder.fromHttpUrl(httpUrl)
+			.build()
+			.encode()
+			.toUri();
+	}
+
+	public static URI fromHttpUrl(String httpUrl, MultiValueMap<String, String> queryParams) {
+		return UriComponentsBuilder.fromHttpUrl(httpUrl)
+			.queryParams(queryParams)
+			.build()
+			.encode()
+			.toUri();
+	}
+
+	public static URI fromHttpUrl(String httpUrl, String path, MultiValueMap<String, String> queryParams) {
+		return UriComponentsBuilder.fromHttpUrl(httpUrl)
+			.path(path)
+			.queryParams(queryParams)
+			.build()
+			.encode()
+			.toUri();
+	}
+}
