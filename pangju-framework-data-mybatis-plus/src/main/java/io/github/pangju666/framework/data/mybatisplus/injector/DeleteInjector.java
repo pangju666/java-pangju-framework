0913fef4787eabfe21b96b6bf5cfd8c46aa1d30e
majiale <2281525307@qq.com>
2024-05-20 15:03:30 +0800

    perf(mybatis-plus): 替换废弃方法

--- pangju-framework-data-mybatis-plus/src/main/java/io/github/pangju666/framework/data/mybatisplus/injector/DeleteInjector.java
+++ pangju-framework-data-mybatis-plus/src/main/java/io/github/pangju666/framework/data/mybatisplus/injector/DeleteInjector.java
@@ -6,19 +6,20 @@ import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import io.github.pangju666.framework.data.mybatisplus.injector.methods.Delete;
 import io.github.pangju666.framework.data.mybatisplus.injector.methods.DeleteBatchByIds;
 import io.github.pangju666.framework.data.mybatisplus.injector.methods.DeleteById;
+import org.apache.ibatis.session.Configuration;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class DeleteInjector extends DefaultSqlInjector {
 	@Override
-	public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+	public List<AbstractMethod> getMethodList(Configuration configuration, Class<?> mapperClass, TableInfo tableInfo) {
 		// 重写的注入要先于父类的注入，否则会导致注入失败
 		List<AbstractMethod> list = new LinkedList<>();
 		list.add(new Delete());
 		list.add(new DeleteBatchByIds());
 		list.add(new DeleteById());
-		list.addAll(super.getMethodList(mapperClass, tableInfo));
+		list.addAll(super.getMethodList(configuration, mapperClass, tableInfo));
 		return list;
 	}
 }
