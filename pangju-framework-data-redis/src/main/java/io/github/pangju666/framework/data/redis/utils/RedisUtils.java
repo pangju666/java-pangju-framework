9e8a64b7cd084df5fec1fd2feaf38e0376d49725
majiale <2281525307@qq.com>
2024-06-24 12:03:31 +0800

    refactor(RedisUtils): 重写RedisUtils

--- pangju-framework-data-redis/src/main/java/io/github/pangju666/framework/data/redis/utils/RedisUtils.java
+++ pangju-framework-data-redis/src/main/java/io/github/pangju666/framework/data/redis/utils/RedisUtils.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,80 +18,50 @@ public class RedisUtils {
     protected RedisUtils() {
     }
 
-    public static String generateKey(final String... element) {
-        return StringUtils.join(Arrays.asList(element), ConstantPool.REDIS_PATH_DELIMITER);
-    }
-
-    public static <K> Set<K> likeRightKeys(final String keyword, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions(keyword + "*", null, null))) {
-            return iterator.stream().collect(Collectors.toSet());
-        }
-    }
-
-    public static <K> Set<K> likeRightKeys(final String keyword, final DataType dataType, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions(keyword + "*", dataType, null))) {
-            return iterator.stream().collect(Collectors.toSet());
-        }
+    public static String computeKey(final String... keys) {
+        return StringUtils.join(Arrays.asList(keys), ConstantPool.REDIS_PATH_DELIMITER);
     }
 
     public static <K> Set<K> likeLeftKeys(final String keyword, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions("*" + keyword, null, null))) {
+        try (var iterator = redisTemplate.scan(likeLeftScanOptions(keyword))) {
             return iterator.stream().collect(Collectors.toSet());
         }
     }
 
-    public static <K> Set<K> likeLeftKeys(final String keyword, final DataType dataType, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions("*" + keyword, dataType, null))) {
+    public static <K> Set<K> likeRightKeys(final String keyword, final RedisTemplate<K, ?> redisTemplate) {
+        try (var iterator = redisTemplate.scan(likeRightScanOptions(keyword))) {
             return iterator.stream().collect(Collectors.toSet());
         }
     }
 
     public static <K> Set<K> likeKeys(final String keyword, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions("*" + keyword + "*", null, null))) {
-            return iterator.stream().collect(Collectors.toSet());
-        }
-    }
-
-    public static <K> Set<K> likeKeys(final String keyword, final DataType dataType, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions("*" + keyword + "*", dataType, null))) {
+        try (var iterator = redisTemplate.scan(likeScanOptions(keyword))) {
             return iterator.stream().collect(Collectors.toSet());
         }
     }
 
     public static <K> Set<K> keys(final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions(null, null, null))) {
+        try (var iterator = redisTemplate.scan(emptyScanOptions())) {
             return iterator.stream().collect(Collectors.toSet());
         }
     }
 
-    public static <K> Set<K> keys(final String pattern, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions(pattern, null, null))) {
+    public static <K> Set<K> keys(final ScanOptions scanOptions, final RedisTemplate<K, ?> redisTemplate) {
+        try (var iterator = redisTemplate.scan(scanOptions)) {
             return iterator.stream().collect(Collectors.toSet());
         }
     }
 
-    public static <K> Set<K> keys(final DataType dataType, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions(null, dataType, null))) {
-            return iterator.stream().collect(Collectors.toSet());
-        }
-    }
-
-    public static <K> Set<K> keys(final String pattern, final DataType dataType, final RedisTemplate<K, ?> redisTemplate) {
-        try (var iterator = redisTemplate.scan(scanOptions(pattern, dataType, null))) {
-            return iterator.stream().collect(Collectors.toSet());
-        }
-    }
-
-    public static <K, V> List<Pair<V, Double>> likeRightZSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForZSet().scan(key, scanOptions(keyword + "*", null, null))) {
+    public static <K, V> List<Pair<V, Double>> likeLeftZSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
+        try (var iterator = redisTemplate.opsForZSet().scan(key, likeLeftScanOptions(keyword))) {
             return iterator.stream()
                     .map(value -> Pair.of(value.getValue(), value.getScore()))
                     .toList();
         }
     }
 
-    public static <K, V> List<Pair<V, Double>> likeLeftZSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForZSet().scan(key, scanOptions("*" + keyword, null, null))) {
+    public static <K, V> List<Pair<V, Double>> likeRightZSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
+        try (var iterator = redisTemplate.opsForZSet().scan(key, likeRightScanOptions(keyword))) {
             return iterator.stream()
                     .map(value -> Pair.of(value.getValue(), value.getScore()))
                     .toList();
@@ -97,15 +69,7 @@ public class RedisUtils {
     }
 
     public static <K, V> List<Pair<V, Double>> likeZSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForZSet().scan(key, scanOptions("*" + keyword + "*", null, null))) {
-            return iterator.stream()
-                    .map(value -> Pair.of(value.getValue(), value.getScore()))
-                    .toList();
-        }
-    }
-
-    public static <K, V> List<Pair<V, Double>> zSetValues(final K key, final String pattern, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForZSet().scan(key, scanOptions(pattern, null, null))) {
+        try (var iterator = redisTemplate.opsForZSet().scan(key, likeScanOptions(keyword))) {
             return iterator.stream()
                     .map(value -> Pair.of(value.getValue(), value.getScore()))
                     .toList();
@@ -113,84 +77,126 @@ public class RedisUtils {
     }
 
     public static <K, V> List<Pair<V, Double>> zSetValues(final K key, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForZSet().scan(key, scanOptions(null, null, null))) {
+        try (var iterator = redisTemplate.opsForZSet().scan(key, emptyScanOptions())) {
             return iterator.stream()
                     .map(value -> Pair.of(value.getValue(), value.getScore()))
                     .toList();
         }
     }
 
-    public static <K, V> Set<V> likeRightSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForSet().scan(key, scanOptions(keyword + "*", null, null))) {
+    public static <K, V> List<Pair<V, Double>> zSetValues(final K key, final ScanOptions scanOptions, final RedisTemplate<K, V> redisTemplate) {
+        try (var iterator = redisTemplate.opsForZSet().scan(key, scanOptions)) {
+            return iterator.stream()
+                    .map(value -> Pair.of(value.getValue(), value.getScore()))
+                    .toList();
+        }
+    }
+
+    public static <K, V> Set<V> likeLeftSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
+        try (var iterator = redisTemplate.opsForSet().scan(key, likeLeftScanOptions(keyword))) {
             return iterator.stream()
                     .collect(Collectors.toSet());
         }
     }
 
-    public static <K, V> Set<V> likeLeftSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForSet().scan(key, scanOptions("*" + keyword, null, null))) {
+    public static <K, V> Set<V> likeRightSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
+        try (var iterator = redisTemplate.opsForSet().scan(key, likeRightScanOptions(keyword))) {
             return iterator.stream()
                     .collect(Collectors.toSet());
         }
     }
 
     public static <K, V> Set<V> likeSetValues(final K key, final String keyword, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForSet().scan(key, scanOptions("*" + keyword + "*", null, null))) {
+        try (var iterator = redisTemplate.opsForSet().scan(key, likeScanOptions(keyword))) {
             return iterator.stream()
                     .collect(Collectors.toSet());
         }
     }
 
     public static <K, V> Set<V> setValues(final K key, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForSet().scan(key, scanOptions(null, null, null))) {
+        try (var iterator = redisTemplate.opsForSet().scan(key, emptyScanOptions())) {
             return iterator.stream()
                     .collect(Collectors.toSet());
         }
     }
 
-    public static <K, V> Set<V> setValues(final K key, final String pattern, final RedisTemplate<K, V> redisTemplate) {
-        try (var iterator = redisTemplate.opsForSet().scan(key, scanOptions(pattern, null, null))) {
+    public static <K, V> Set<V> setValues(final K key, final ScanOptions scanOptions, final RedisTemplate<K, V> redisTemplate) {
+        try (var iterator = redisTemplate.opsForSet().scan(key, scanOptions)) {
             return iterator.stream()
                     .collect(Collectors.toSet());
-        }
-    }
-
-    public static <K, HK, HV> Map<HK, HV> likeRightHashValues(final K key, final String keyword, final RedisTemplate<K, ?> redisTemplate) {
-        HashOperations<K, HK, HV> hashOperations = redisTemplate.opsForHash();
-        try (Cursor<Map.Entry<HK, HV>> iterator = hashOperations.scan(key, scanOptions(keyword + "*", null, null))) {
-            return toMap(iterator.stream().toList());
         }
     }
 
     public static <K, HK, HV> Map<HK, HV> likeLeftHashValues(final K key, final String keyword, final RedisTemplate<K, ?> redisTemplate) {
         HashOperations<K, HK, HV> hashOperations = redisTemplate.opsForHash();
-        try (var iterator = hashOperations.scan(key, scanOptions("*" + keyword, null, null))) {
+        try (var iterator = hashOperations.scan(key, likeLeftScanOptions(keyword))) {
+            return toMap(iterator.stream().toList());
+        }
+    }
+
+    public static <K, HK, HV> Map<HK, HV> likeRightHashValues(final K key, final String keyword, final RedisTemplate<K, ?> redisTemplate) {
+        HashOperations<K, HK, HV> hashOperations = redisTemplate.opsForHash();
+        try (Cursor<Map.Entry<HK, HV>> iterator = hashOperations.scan(key, likeRightScanOptions(keyword))) {
             return toMap(iterator.stream().toList());
         }
     }
 
     public static <K, HK, HV> Map<HK, HV> likeHashValues(final K key, final String keyword, final RedisTemplate<K, ?> redisTemplate) {
         HashOperations<K, HK, HV> hashOperations = redisTemplate.opsForHash();
-        try (var iterator = hashOperations.scan(key, scanOptions("*" + keyword + "*", null, null))) {
+        try (var iterator = hashOperations.scan(key, likeScanOptions(keyword))) {
             return toMap(iterator.stream().toList());
         }
     }
 
     public static <K, HK, HV> Map<HK, HV> hashValues(final K key, final RedisTemplate<K, ?> redisTemplate) {
         HashOperations<K, HK, HV> hashOperations = redisTemplate.opsForHash();
-        try (var iterator = hashOperations.scan(key, scanOptions(null, null, null))) {
+        try (var iterator = hashOperations.scan(key, emptyScanOptions())) {
             return toMap(iterator.stream().toList());
         }
     }
 
-    public static <K, HK, HV> Map<HK, HV> hashValues(final K key, final String pattern, final RedisTemplate<K, ?> redisTemplate) {
+    public static <K, HK, HV> Map<HK, HV> hashValues(final K key, final ScanOptions scanOptions, final RedisTemplate<K, ?> redisTemplate) {
         HashOperations<K, HK, HV> hashOperations = redisTemplate.opsForHash();
-        try (var iterator = hashOperations.scan(key, scanOptions(pattern, null, null))) {
+        try (var iterator = hashOperations.scan(key, scanOptions)) {
             return toMap(iterator.stream().toList());
         }
     }
 
-    public static ScanOptions scanOptions(final String pattern, final DataType dataType, final Long count) {
+    public static ScanOptions likeLeftScanOptions(final String keyword) {
+        Assert.hasText(keyword, "keyword不可为空");
+        return scanOptions("*" + keyword, null, null);
+    }
+
+    public static ScanOptions likeLeftScanOptions(final String keyword, @Nullable final DataType dataType, @Nullable final Long count) {
+        Assert.hasText(keyword, "keyword不可为空");
+        return scanOptions("*" + keyword, dataType, count);
+    }
+
+    public static ScanOptions likeRightScanOptions(final String keyword) {
+        Assert.hasText(keyword, "keyword不可为空");
+        return scanOptions(keyword + "*", null, null);
+    }
+
+    public static ScanOptions likeRightScanOptions(final String keyword, @Nullable final DataType dataType, @Nullable final Long count) {
+        Assert.hasText(keyword, "keyword不可为空");
+        return scanOptions(keyword + "*", dataType, count);
+    }
+
+    public static ScanOptions likeScanOptions(final String keyword) {
+        Assert.hasText(keyword, "keyword不可为空");
+        return scanOptions("*" + keyword + "*", null, null);
+    }
+
+    public static ScanOptions likeScanOptions(final String keyword, @Nullable final DataType dataType, @Nullable final Long count) {
+        Assert.hasText(keyword, "keyword不可为空");
+        return scanOptions("*" + keyword + "*", dataType, count);
+    }
+
+    public static ScanOptions emptyScanOptions() {
+        return scanOptions(null, null, null);
+    }
+
+    public static ScanOptions scanOptions(@Nullable final String pattern, @Nullable final DataType dataType, @Nullable final Long count) {
         var builder = ScanOptions.scanOptions();
         if (Objects.nonNull(count)) {
             builder.count(count);
